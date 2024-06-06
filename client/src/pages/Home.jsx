2a5ea52bc7557488d3d9c5cc5c65a1d6795b2121
arbaz khan <itsarbaz51@gmail.com>
2024-06-06 23:52:40 +0530

    Add Footer

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,16 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit incidunt autem, consectetur in exercitationem vel! Placeat doloribus eum aut maxime nobis accusamus qui. Dolorem tenetur aliquam alias quos corrupti commodi?
-    Iure eveniet corrupti fugiat facilis ipsa, minima explicabo? Eos repellat perferendis ea neque odit, esse aspernatur explicabo modi itaque, accusamus iure hic. Quos itaque nostrum est repellat dolore, impedit provident.
-    Rerum in adipisci voluptatem ullam illo veniam hic quas sunt voluptas iure dolorem iste laborum saepe exercitationem qui molestiae impedit odio aut distinctio, officiis eius. Odit tenetur commodi ab adipisci?
-    Deserunt possimus magnam doloremque, fuga reiciendis vel ea temporibus autem reprehenderit neque tempora culpa, animi rerum quaerat inventore modi nisi repellendus nobis corrupti et distinctio ipsa deleniti tenetur? Illum, voluptates!
-    Labore culpa temporibus mollitia harum, aspernatur obcaecati! Corporis consequuntur porro fugit illum facere totam asperiores voluptatum neque qui debitis quos et nesciunt alias, odit reiciendis tempora nostrum aspernatur at quas?
-    Asperiores, expedita nisi quasi iste saepe similique, consectetur at pariatur magni tempora aspernatur nostrum voluptates fuga dolorum. Exercitationem reiciendis, aliquid architecto voluptatem tenetur quod perspiciatis repellat animi odio dignissimos quasi?
-    Tempore quas cum soluta eos iusto, accusamus nemo sed illum. Eveniet, culpa? Possimus nostrum sit error mollitia quos aperiam minima cupiditate in dicta neque, voluptates ad, quibusdam ea cum sapiente!
-    Soluta sed ut corporis quisquam repellat similique natus deserunt ducimus omnis, est rem, voluptatibus architecto necessitatibus. Ullam, aliquam! Id placeat aut doloremque deserunt autem, eveniet odit veniam at nemo. Tempore!
-    Cumque rem consequuntur dolorum illum cupiditate distinctio omnis. Molestias numquam enim ratione quam ipsa rerum eligendi? Culpa, voluptatem labore sint dolorum, porro saepe officiis ipsa, molestias quaerat soluta similique consectetur?
-    Error libero voluptatibus sint repellat aperiam consequuntur molestiae? Dolore eius suscipit culpa sed deserunt quas ex consectetur ducimus optio aliquid asperiores, labore ut accusantium vero. Ex numquam tempore officia delectus.</div>
+    <div>
+      home
+    </div>
   )
 }
 
