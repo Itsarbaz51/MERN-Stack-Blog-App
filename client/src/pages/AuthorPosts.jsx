8a8af4daf82c183d881aed0a1d212e0e47648b23
arbaz khan <itsarbaz51@gmail.com>
2024-06-10 14:53:00 +0530

    Add author posts

--- client/src/pages/AuthorPosts.jsx
+++ client/src/pages/AuthorPosts.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import PostItem from '../components/PostItem.jsx'
 import {Dumy_Posts} from '../dumyPosts/data.js'
 function AuthorPost() {
