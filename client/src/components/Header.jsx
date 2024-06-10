e7f56c67d6adc841fea1724861e8b6f57bf23b27
arbaz khan <itsarbaz51@gmail.com>
2024-06-10 14:48:47 +0530

    Add heade and Navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,39 +1,84 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { IoMenuSharp } from "react-icons/io5";
 import { IoClose } from "react-icons/io5";
 
 function Header() {
+  const [isNavShowing, setIsNaveShowing] = useState(
+    window.innerWidth > 800 ? true : false
+  );
 
+  const closeNavHandle = () => {
+    if (window.innerWidth < 800) {
+      setIsNaveShowing(false);
+    } else {
+      setIsNaveShowing(true);
+    }
+  };
   return (
     <>
       <nav>
-        <div className="flex justify-between border-b-2 p-3 m-2">
+        <div className="bg-slate-50 rounded w-screen  flex justify-between p-3">
           <div>
             <h3>
-              <Link to="/" >Blog App</Link>
+              <Link to="/" onClick={closeNavHandle}>
+                Blog App
+              </Link>
             </h3>
           </div>
-          <div className="hidden sm:flex">
-            <ul className="flex md:gap-8  rounded-sm sm:gap-3">
+          <div>
+            {isNavShowing && (
+              <ul className="bg-gray-50 z-10 absolute right-5 top-16 p-4 shadow-2xl grid gap-3 underline rounded-md sm:hidden ">
+                <li>
+                  <Link to="/profile/arbaz" onClick={closeNavHandle}>
+                    Arbaz
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/create" onClick={closeNavHandle}>
+                    Create Post
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/authors" onClick={closeNavHandle}>
+                    Authors
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/logout" onClick={closeNavHandle}>
+                    Logout
+                  </Link>
+                </li>
+              </ul>
+            )}
+            <ul className="sm:flex gap-4 md:gap-5 hidden ">
               <li>
-                <Link to="/profile/arbaz">Arbaz</Link>
+                <Link to="/profile/arbaz" onClick={closeNavHandle}>
+                  Arbaz
+                </Link>
               </li>
               <li>
-                <Link to="/create">Create Post</Link>
+                <Link to="/create" onClick={closeNavHandle}>
+                  Create Post
+                </Link>
               </li>
               <li>
-                <Link to="/authors">Authors</Link>
+                <Link to="/authors" onClick={closeNavHandle}>
+                  Authors
+                </Link>
               </li>
               <li>
-                <Link to="/logout">Logout</Link>
+                <Link to="/logout" onClick={closeNavHandle}>
+                  Logout
+                </Link>
               </li>
             </ul>
           </div>
           <button
-            className="sm:hidden hover:bg-slate-100 hover:rounded"
+            className="sm:hidden lg:hidden outline-none"
+            onClick={() => setIsNaveShowing(!isNavShowing)}
           >
-                            <IoMenuSharp /> 
+            {isNavShowing ? <IoClose /> : <IoMenuSharp />}
           </button>
         </div>
       </nav>
