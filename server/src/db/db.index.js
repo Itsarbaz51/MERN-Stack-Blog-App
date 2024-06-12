e3cd9210d329e44916863b94e5beea7d43aec78a
arbaz khan <itsarbaz51@gmail.com>
2024-06-12 17:50:49 +0530

    connect db use mongoose

--- server/src/db/db.index.js
+++ server/src/db/db.index.js
@@ -3,7 +3,7 @@ import DB_NAME from '../constants.js'
 
 const connectDB = async () => {
     try {
-        const res = await mongoose.connect(`${process.env.MONGODB_URL}/ ${DB_NAME}`)
+        const res = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
         console.log(`MMONGODB connect successfully || DB host ${res.connection.host}`);
     } catch (error) {
         console.log('MONGO db can not connect',error.message);
