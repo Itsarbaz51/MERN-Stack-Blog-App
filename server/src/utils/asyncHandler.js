d8594a084bd27408174c7b0d3afab3a96597fc2e
arbaz khan <itsarbaz51@gmail.com>
2024-06-26 15:08:27 +0530

    setup for send user request handler

--- server/src/utils/asyncHandler.js
+++ server/src/utils/asyncHandler.js
@@ -1,7 +1,9 @@
-const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error))
-    }
-}
+const asyncHandler = (requestHandle) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandle(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
 
-export default asyncHandler
+export { asyncHandler };
