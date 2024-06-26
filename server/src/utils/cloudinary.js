443d72bb645b584a26f5bb00e2b7463fc7ccc6a7
arbaz khan <itsarbaz51@gmail.com>
2024-06-26 15:06:37 +0530

    Avatar and Thumbnail files upload on cloudinary setup.

---
+++ server/src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+
+        if(!localFilePath) return "File not found." || null
+
+        const res = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        })
+        fs.unlinkSync(localFilePath)
+        return res
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+export {
+    uploadOnCloudinary
+}
