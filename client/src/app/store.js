ad3783e22d5cf78ecc2bb9c7e4c4fe1a1269472c
arbaz khan <itsarbaz51@gmail.com>
2024-07-29 14:13:18 +0530

    create store for using reduxtoolkit

---
+++ client/src/app/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducers from "../slices/authSlice.js";
+import postReducers from "../slices/postSlice.js";
+
+export const store = configureStore({
+  reducer: { 
+    auth: authReducers,
+    posts: postReducers,
+  },
+  
+});
