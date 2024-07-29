d933ae62fc05a879b2a7ce9a541c0beb34eaa920
arbaz khan <itsarbaz51@gmail.com>
2024-07-29 14:55:57 +0530

    fetch backend data

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,14 +1,33 @@
-import React, { useState } from "react";
-import {Link} from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { register } from "../slices/authSlice.js";
+import { useDispatch, useSelector } from "react-redux";
 
 function Register() {
   const [userData, setUserdata] = useState({
-    name: "",
+    fullName: "",
     email: "",
     password: "",
     confirmPassword: "",
   });
 
+  const dispatch = useDispatch();
+  const { registerUser, error} = useSelector(state => state.auth)
+  const navigate = useNavigate();
+
+  const handleSubmitRegister = async (e) => {
+    e.preventDefault();
+    dispatch(register(userData));
+    setUserdata("");
+  };
+  
+  useEffect(() => {
+    registerUser?.message ? navigate('/login') : null
+  });
+  
+  if (registerUser?.data) {
+    alert("Register Success.");
+  }
   const changeInputHandle = (e) => {
     setUserdata((prevState) => {
       return {
@@ -22,15 +41,20 @@ function Register() {
     <section className="flex justify-center  bg-slate-300">
       <div className="bg-slate-100 rounded center md:w-[500px] my-44 p-4 h-fit min-w-fit ">
         <h1 className="font-bold ms-2 ">Sign Up</h1>
-        <form className="flex flex-col p-2">
-          <p className="bg-red-500 rounded mb-3 text-[15px] p-1 sm:text-xs text-white">This is error Message</p>
+        <form className="flex flex-col p-2" onSubmit={handleSubmitRegister}>
+          { error?.error &&
+            <p className="bg-red-500 rounded mb-3 text-[15px] p-1 sm:text-xs text-white">
+              {error?.error}
+            </p>
+          }
           <input
             type="text"
             placeholder="Full Name"
-            name="name"
-            value={userData.name}
+            name="fullName"
+            value={userData.fullName}
             onChange={changeInputHandle}
             className="p-2 mb-2 rounded-lg outline-none"
+            
           />
           <input
             type="email"
@@ -39,7 +63,6 @@ function Register() {
             value={userData.email}
             onChange={changeInputHandle}
             className="p-2 mb-2 rounded-lg outline-none"
-
           />
           <input
             type="password"
@@ -48,7 +71,6 @@ function Register() {
             value={userData.password}
             onChange={changeInputHandle}
             className="p-2 mb-2 rounded-lg outline-none"
-
           />
           <input
             type="password"
@@ -57,13 +79,21 @@ function Register() {
             value={userData.confirmPassword}
             onChange={changeInputHandle}
             className="p-2 mb-2 rounded-lg outline-none"
-
           />
-          <button 
-          type="submit"
-          className="bg-purple-700 rounded-md text-[17px] text-white text-center w-fit p-2 hover:bg-purple-800">Register</button>
+          <button
+            type="submit"
+            className="bg-purple-700 rounded-md text-[17px] text-white text-center w-fit p-2 hover:bg-purple-800"
+          >
+            Register
+          </button>
         </form>
-        <small className="text-[15px] text-gray-500 flex justify-center">Already have an account? <Link to='/login' className="text-blue-800 ml-1"> sign in</Link></small>
+        <small className="text-[15px] text-gray-500 flex justify-center">
+          Already have an account?{" "}
+          <Link to="/login" className="text-blue-800 ml-1">
+            {" "}
+            sign in
+          </Link>
+        </small>
       </div>
     </section>
   );
