be99711340cd4a2cafe4d486b3a5219cc9bf82fa
arbaz khan <itsarbaz51@gmail.com>
2024-07-31 18:11:25 +0530

    Added authentication middleware

--- server/src/middlewares/Auth.middleware.js
+++ server/src/middlewares/Auth.middleware.js
@@ -5,27 +5,22 @@ import jwt from 'jsonwebtoken'
 
 
 const verifyJWT = asyncHandler(async (req , _, next) => {
-    // console.log(req.cookies?.accessToken);
     try {
         const token = await req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-        // console.log(token);
         if (!token) {
             throw new ApiError(401, "Unauthorized request.")
         }
 
         const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-        // console.log("decoded.",decoded._id);
         const user = await User.findById(decoded?._id).select("-password -refreshToken")
-        // console.log("user details", user);
 
         if (!user) {
             throw new ApiError(401, "Invaild Access Token user.")
         }
         req.user = user
-        // console.log(user);
         next()
     } catch (error) {
-        throw new ApiError(401, error.message || "Invaid Access Token.")
+        throw new ApiError(401, error?.message || "Invaid Access Token.")
     }
 })
 
