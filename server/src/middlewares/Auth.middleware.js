f2cf07a0861f11aa97f636a237468b3096f625fc
arbaz khan <itsarbaz51@gmail.com>
2024-08-27 16:14:06 +0530

    Add auth middleware

--- server/src/middlewares/Auth.middleware.js
+++ server/src/middlewares/Auth.middleware.js
@@ -7,11 +7,12 @@ import jwt from 'jsonwebtoken'
 const verifyJWT = asyncHandler(async (req , _, next) => {
     try {
         const token = await req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-        if (!token) {
+        const userToken = token.split(" ")[0]
+        if (!userToken) {
             throw new ApiError(401, "Unauthorized request.")
         }
 
-        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const decoded = jwt.verify(userToken, process.env.ACCESS_TOKEN_SECRET)
         const user = await User.findById(decoded?._id).select("-password -refreshToken")
 
         if (!user) {
