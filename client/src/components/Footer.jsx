c525454071714374d8950d62edced13500770954
arbaz khan <itsarbaz51@gmail.com>
2024-08-27 16:07:01 +0530

    Add footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,30 +3,30 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer>
-      <div className="bg-slate-100 hover:shadow-2xl hover:shadow-slate-300 p-4 m-3  rounded-3xl">
+      <div className="bg-gray-50 hover:shadow-2xl p-4 border-t-2 border-gray-400">
       <ul className="text-center p-2 flex flex-wrap justify-evenly gap-3 ">
-        <li className="hover:bg-black hover:text-white rounded hover:inline-block p-2">
+        <li className="transition duration-700 ease-out hover:bg-black hover:text-white rounded hover:inline-block p-2">
           <Link to="/posts/categories/Agriclture">Agriclture</Link>
         </li>
-        <li className=" hover:bg-black hover:text-white rounded hover:inline-block p-2">
+        <li className="transition duration-700 ease-out hover:bg-black hover:text-white rounded hover:inline-block p-2">
           <Link to="/posts/categories/Business">Business</Link>
         </li>
-        <li className="hover:bg-black hover:text-white rounded hover:inline-block p-2">
+        <li className="transition duration-700 ease-out hover:bg-black hover:text-white rounded hover:inline-block p-2">
           <Link to="/posts/categories/Education">Education</Link>
         </li>
-        <li className=" hover:bg-black hover:text-white rounded hover:inline-block p-2">
+        <li className="transition duration-700 ease-out hover:bg-black hover:text-white rounded hover:inline-block p-2">
           <Link to="/posts/categories/Entertainment">Entertainment</Link>
         </li>
-        <li className=" hover:bg-black hover:text-white rounded hover:inline-block p-2">
+        <li className="transition duration-700 ease-out hover:bg-black hover:text-white rounded hover:inline-block p-2">
           <Link to="/posts/categories/Art">Art</Link>
         </li>
-        <li className=" hover:bg-black hover:text-white rounded hover:inline-block p-2">
+        <li className="transition duration-700 ease-out hover:bg-black hover:text-white rounded hover:inline-block p-2">
           <Link to="/posts/categories/Invester">Invester</Link>
         </li>
-        <li className=" hover:bg-black hover:text-white rounded hover:inline-block p-2">
+        <li className="transition duration-700 ease-out hover:bg-black hover:text-white rounded hover:inline-block p-2">
           <Link to="/posts/categories/Uncotegorized">Uncotegorized</Link>
         </li>
-        <li className=" hover:bg-black hover:text-white rounded hover:inline-block p-2">
+        <li className="transition duration-700 ease-out hover:bg-black hover:text-white rounded hover:inline-block p-2">
           <Link to="/posts/categories/Weather">Weather</Link>
         </li>
       </ul>
