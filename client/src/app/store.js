a44ce6dd40d25e46616477f58c2371a59dc1d512
arbaz khan <itsarbaz51@gmail.com>
2024-08-27 16:09:55 +0530

    Add store use redux toolkit

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,11 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers} from "@reduxjs/toolkit";
+// import {persistStore, persistReducer} from 'redux-persist'
+// import storage from "redux-persist/lib/storage";
+
+
 import authReducers from "../slices/authSlice.js";
 import postReducers from "../slices/postSlice.js";
 
+// const rootReducer = combineReducers({
+//   auth: authReducers,
+//   posts: postReducers,
+// });
+
+// const persistConfig = {
+//   key: 'root',
+//   storage
+//   // whitelist: ['auth', 'posts']
+// }
+
+// const persistedReducer  = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-  reducer: { 
+  // reducer: persistedReducer,
+  reducer: {
     auth: authReducers,
-    posts: postReducers,
-  },
-  
-});
+    posts: postReducers
+  }
+})
+
+// export const persistor = persistStore(store)
+
